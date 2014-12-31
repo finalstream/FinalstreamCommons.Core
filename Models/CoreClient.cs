bb62b9eeedf0d3c3389a639a8516dac05867255e
finalstream <info@finalstream.net>
2015-01-01 01:55:30 +0900

    とりあえずGroupGridの表示までできた。

---
+++ Models/CoreClient.cs
@@ -0,0 +1,26 @@
+﻿using NLog;
+
+namespace FinalstreamCommons.Models
+{
+    public abstract class CoreClient
+    {
+
+        private Logger _logger = LogManager.GetCurrentClassLogger();
+
+        protected CoreClient()
+        {
+        }
+
+        /// <summary>
+        /// 初期化を行います。
+        /// </summary>
+        public void Initialize()
+        {
+            InitializeCore();
+        }
+
+        protected abstract void InitializeCore();
+
+
+    }
+}
