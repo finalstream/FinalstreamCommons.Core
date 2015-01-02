ec603cefb687f326cd58c3ca69c932ad7b99fb59
finalstream <info@finalstream.net>
2015-01-02 20:54:00 +0900

    設定ファイルを実装。

--- Models/CoreClient.cs
+++ Models/CoreClient.cs
@@ -1,29 +1,48 @@
-﻿using NLog;
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace FinalstreamCommons.Models
 {
     public abstract class CoreClient
     {
-
-        private Logger _logger = LogManager.GetCurrentClassLogger();
-
-        protected CoreClient()
-        {
-        }
-
         /// <summary>
         /// 初期化を行います。
         /// </summary>
         public void Initialize()
         {
             InitializeCore();
+        }
 
+        /// <summary>
+        /// 終了処理を行います。
+        /// </summary>
+        public void Finish()
+        {
+            FinalizeCore();
+        }
 
+        /// <summary>
+        /// 設定をファイルからロードします。
+        /// </summary>
+        protected T LoadConfig<T>(string configFilePath) where T : IAppConfig, new()
+        {
+            if (!File.Exists(configFilePath)) return new T();
+            return JsonConvert.DeserializeObject<T>(
+                File.ReadAllText(configFilePath));
+        }
 
+        /// <summary>
+        /// 設定をファイルに保存します。
+        /// </summary>
+        protected void SaveConfig<T>(string configFilePath, T config) where T : IAppConfig, new()
+        {
+            if (configFilePath == null) return;
+            Directory.CreateDirectory(Path.GetDirectoryName(configFilePath));
+            File.WriteAllText(configFilePath, JsonConvert.SerializeObject(config, Formatting.Indented));
         }
 
         protected abstract void InitializeCore();
-
-
+        protected abstract void FinalizeCore();
     }
 }
---
+++ Models/IAppConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows;
+
+namespace FinalstreamCommons.Models
+{
+    /// <summary>
+    /// アプリケーション設定を表します。
+    /// </summary>
+    public interface IAppConfig
+    {
+        /// <summary>
+        /// アプリバージョンを取得します。
+        /// </summary>
+        string AppVersion { get; }
+
+        /// <summary>
+        /// ウインドウの位置、大きさを取得します。
+        /// </summary>
+        Rect WindowRect { get; }
+
+    }
+}
