969409fe4dce7ae839605a72a2fc4b8ddd8892a6
finalstream <info@finalstream.net>
2015-01-04 16:37:30 +0900

    コンボの実装をリファクタ。

---
+++ Extentions/CollectionExtentions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalstreamCommons.Extentions
+{
+    public static class CollectionExtentions
+    {
+        public static void DiffUpdate<T>(this ICollection<T> nowCollection, ICollection<T> newCollection)
+        {
+            if (nowCollection.Count == 0) nowCollection.Clear();    // クリアしないとなぜか重複するので。
+
+            var removes = nowCollection.Except(newCollection);
+
+            var adds = newCollection.Except(nowCollection);
+
+            foreach (var remove in removes)
+            {
+                nowCollection.Remove(remove);
+            }
+
+            foreach (var add in adds)
+            {
+                if (!nowCollection.Contains(add))nowCollection.Add(add);
+            }
+        }
+    }
+}
