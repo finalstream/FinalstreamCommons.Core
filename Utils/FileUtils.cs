4d289c1590f7b4105eb005eecd694cb14ee0204a
finalstream <info@finalstream.net>
2015-01-18 20:58:48 +0900

    グループを表示できるようにした。

--- Utils/FileUtils.cs
+++ Utils/FileUtils.cs
@@ -9,6 +9,19 @@ namespace FinalstreamCommons.Utils
     public static class FileUtils
     {
 
+        /// <summary>
+        /// ファイルサイズをGBの文字列に変換します。
+        /// </summary>
+        /// <param name="fileSize">変換元のファイルサイズ。</param>
+        /// <returns>変換後のファイルサイズ。</returns>
+        public static string ConvertFileSizeGigaByteString(long fileSize)
+        {
+            if (fileSize == 0)
+            {
+                return "";
+            }
 
+            return String.Format("{0:##0.#0}", fileSize / 1073741824.0) + " GB";
+        }
     }
 }
