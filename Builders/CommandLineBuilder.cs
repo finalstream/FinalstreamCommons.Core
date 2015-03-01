6a2143fd1833c22e73810448bcaadcdf9d302800
finalstream <info@finalstream.net>
2015-03-01 10:07:17 +0900

    スロー機能を実装した。

---
+++ Builders/CommandLineBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalstreamCommons.Builders
+{
+    public class CommandLineBuilder : Microsoft.Build.Utilities.CommandLineBuilder
+    {
+
+        /// <summary>
+        /// クリアします。
+        /// </summary>
+        public void Clear()
+        {
+            CommandLine.Clear();
+        }
+    }
+}
