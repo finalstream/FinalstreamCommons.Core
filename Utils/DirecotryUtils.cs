255f3719af114a60b1d0a5f97a0123994d3c5ba1
finalstream <info@finalstream.net>
2015-03-29 21:00:40 +0900

    グループ移動機能を実装した。

---
+++ Utils/DirecotryUtils.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalstreamCommons.Utils
+{
+    /// <summary>
+    /// ディレクトリを扱うユーティリティです。
+    /// </summary>
+    public static class DirecotryUtils
+    {
+
+        /// <summary>
+        /// ディレクトリが空の場合、削除します。
+        /// </summary>
+        /// <param name="direcotry"></param>
+        /// <returns></returns>
+        public static bool DeleteEmptyDirecotry(string direcotry)
+        {
+            if (!IsEmptyDirectory(direcotry)) return false;
+
+            Directory.Delete(direcotry);
+
+            return true;
+        }
+
+        /// <summary>
+        /// ディレクトリが空であるかどうか。
+        /// </summary>
+        /// <param name="direcotry"></param>
+        /// <returns></returns>
+        public static bool IsEmptyDirectory(string direcotry)
+        {
+            if (!Directory.Exists(direcotry)) return false; // ディレクトリが存在しなければ空でないとする
+  
+            try
+            {
+                var files = Directory.GetFileSystemEntries(direcotry);
+                return files.Length == 0;
+            }
+            catch
+            {
+                // アクセス権がないなどの場合は空でないとする
+                return false;
+            }
+        }
+    }
+}
--- Utils/FileUtils.cs
+++ Utils/FileUtils.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -36,6 +37,28 @@ namespace FinalstreamCommons.Utils
         public static long GetFileSize(string filePath)
         {
             return new FileInfo(filePath).Length;
+        }
+
+        /// <summary>
+        /// ファイルを移動します。移動先にすでに同名のファイルが存在する場合は何もしません。
+        /// </summary>
+        /// <param name="srcPath"></param>
+        /// <param name="destPath"></param>
+        /// <returns>移動元と移動先が同じか、移動先にすでに同名のファイルが存在する場合はfalse。成功したらtrue。</returns>
+        /// <remarks></remarks>
+        public static bool Move(string srcPath, string destPath)
+        {
+            if (srcPath == destPath || File.Exists(destPath)) return false; // 元と同じか存在したら何もしない。
+
+            File.Move(srcPath, destPath);
+            return true;
+        }
+
+        public static string GetDriveLetter(string filePath)
+        {
+            if (string.IsNullOrEmpty(filePath)) return "";
+
+            return filePath.Substring(0, 1).ToUpper();
         }
     }
 }
