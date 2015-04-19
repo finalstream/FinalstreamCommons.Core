508ce29ec342a5749133f89fde2b2010f0593381
finalstream <info@finalstream.net>
2015-04-19 11:53:36 +0900

    win32apiを修正。

--- Utils/ScreenUtils.cs
+++ Utils/ScreenUtils.cs
@@ -4,62 +4,25 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using FinalstreamCommons.Windows;
 
 namespace FinalstreamCommons.Utils
 {
     public static class ScreenUtils
     {
 
-        [DllImport("user32.dll")]
-        static extern bool EnumDisplayDevices(string lpDevice, uint iDevNum, ref DISPLAY_DEVICE lpDisplayDevice, uint dwFlags);
-
-        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
-        public struct DISPLAY_DEVICE
-        {
-            [MarshalAs(UnmanagedType.U4)]
-            public int cb;
-            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
-            public string DeviceName;
-            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
-            public string DeviceString;
-            [MarshalAs(UnmanagedType.U4)]
-            public DisplayDeviceStateFlags StateFlags;
-            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
-            public string DeviceID;
-            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
-            public string DeviceKey;
-        }
-
-        public enum DisplayDeviceStateFlags : int
-        {
-            /// <summary>The device is part of the desktop.</summary>
-            AttachedToDesktop = 0x1,
-            MultiDriver = 0x2,
-            /// <summary>The device is part of the desktop.</summary>
-            PrimaryDevice = 0x4,
-            /// <summary>Represents a pseudo device used to mirror application drawing for remoting or other purposes.</summary>
-            MirroringDriver = 0x8,
-            /// <summary>The device is VGA compatible.</summary>
-            VGACompatible = 0x10,
-            /// <summary>The device is removable; it cannot be the primary display.</summary>
-            Removable = 0x20,
-            /// <summary>The device has more display modes than its output devices support.</summary>
-            ModesPruned = 0x8000000,
-            Remote = 0x4000000,
-            Disconnect = 0x2000000
-        }
-
         /// <summary>
         /// デバイス名からディスプレイデバイス情報を取得します。
         /// </summary>
         /// <param name="deviceName"></param>
         /// <returns></returns>
-        public static DISPLAY_DEVICE GetDisplayDevice(string deviceName)
+        public static Win32Api.DISPLAY_DEVICE GetDisplayDevice(string deviceName)
         {
-            var d = new DISPLAY_DEVICE();
+            var d = new Win32Api.DISPLAY_DEVICE();
             d.cb = Marshal.SizeOf(d);
-            EnumDisplayDevices(deviceName, 0, ref d, 0);
+            Win32Api.EnumDisplayDevices(deviceName, 0, ref d, 0);
             return d;
         }
+
     }
 }
