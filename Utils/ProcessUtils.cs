e7b7fcd6d788c83471d6805832a60c3b473ada88
finalstream <info@finalstream.net>
2015-11-23 10:11:08 +0900

    プロセスIDから実行ファイルパスを取得するメソッドを追加した。

---
+++ Utils/ProcessUtils.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Management;
+
+namespace FinalstreamCommons.Utils
+{
+    public static class ProcessUtils
+    {
+
+        /// <summary>
+        /// プロセスIDから実行ファイルパスを取得します。
+        /// </summary>
+        /// <param name="processId"></param>
+        /// <returns></returns>
+        public static string GetExecutablePath(int processId)
+        {
+            var wmiQueryString = "SELECT ExecutablePath FROM Win32_Process WHERE ProcessId = " + processId;
+            using (var searcher = new ManagementObjectSearcher(wmiQueryString))
+            {
+                using (var results = searcher.Get())
+                {
+                    var mo = results.Cast<ManagementObject>().FirstOrDefault();
+                    if (mo != null)
+                    {
+                        return (string)mo["ExecutablePath"];
+                    }
+                }
+            }
+            return null;
+        }
+
+    }
+}
