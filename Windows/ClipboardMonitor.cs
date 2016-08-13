41ac8c9646ade15adcd89cd6985a9a9e8d052a28
ふぁいなる <info@finalstream.net>
2016-08-13 21:22:37 +0900

    クリップボード監視クラスを追加。

---
+++ Windows/ClipboardMonitor.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace FinalstreamCommons.Windows
+{
+    /// <summary>
+    /// クリップボードを監視するクラスを表します。
+    /// </summary>
+    internal class ClipboardMonitor : Control
+    {
+        private IntPtr _nextClipboardViewer;
+
+        public ClipboardMonitor()
+        {
+            Visible = false;
+
+            _nextClipboardViewer = (IntPtr) SetClipboardViewer((int) Handle);
+        }
+
+        /// <summary>
+        ///     Clipboard contents changed.
+        /// </summary>
+        public event EventHandler<ClipboardChangedEventArgs> ClipboardChanged;
+
+        protected override void Dispose(bool disposing)
+        {
+            ChangeClipboardChain(Handle, _nextClipboardViewer);
+        }
+
+        [DllImport("User32.dll")]
+        protected static extern int SetClipboardViewer(int hWndNewViewer);
+
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern bool ChangeClipboardChain(IntPtr hWndRemove, IntPtr hWndNewNext);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int SendMessage(IntPtr hwnd, int wMsg, IntPtr wParam, IntPtr lParam);
+
+        protected override void WndProc(ref Message m)
+        {
+            // defined in winuser.h
+            const int WM_DRAWCLIPBOARD = 0x308;
+            const int WM_CHANGECBCHAIN = 0x030D;
+
+            switch (m.Msg)
+            {
+                case WM_DRAWCLIPBOARD:
+                    OnClipboardChanged();
+                    SendMessage(_nextClipboardViewer, m.Msg, m.WParam, m.LParam);
+                    break;
+
+                case WM_CHANGECBCHAIN:
+                    if (m.WParam == _nextClipboardViewer)
+                        _nextClipboardViewer = m.LParam;
+                    else
+                        SendMessage(_nextClipboardViewer, m.Msg, m.WParam, m.LParam);
+                    break;
+
+                default:
+                    base.WndProc(ref m);
+                    break;
+            }
+        }
+
+        private void OnClipboardChanged()
+        {
+            try
+            {
+                var iData = Clipboard.GetDataObject();
+                if (ClipboardChanged != null)
+                {
+                    ClipboardChanged(this, new ClipboardChangedEventArgs(iData));
+                }
+            }
+            catch (Exception)
+            {
+                // エラーが起きたら捨てる
+            }
+        }
+    }
+
+    public class ClipboardChangedEventArgs : EventArgs
+    {
+        public readonly IDataObject DataObject;
+
+        public ClipboardChangedEventArgs(IDataObject dataObject)
+        {
+            DataObject = dataObject;
+        }
+    }
+}
